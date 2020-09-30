7b12b5d64eaf53566616a9a3d17800af939cf80d
surinder2000 <surinder20002017@gmail.com>
2020-09-30 17:01:48 +0530

    modified

--- aws_efs/efs.tf
+++ aws_efs/efs.tf
@@ -1,9 +1,10 @@
+// Configure provider
 provider "aws" {
     region = "ap-south-1"
     profile = "myprofile"
 }
 
-
+// Create Security Group
 resource "aws_security_group" "efsSecurityGroup" {
     name        = "efs-allow"
     description = "NFS access"
@@ -42,6 +43,7 @@ resource "aws_security_group" "efsSecurityGroup" {
     }
 }
 
+// Generate Key
 resource "tls_private_key" "keyGenerate" {
     algorithm = "RSA"
 }
@@ -56,6 +58,8 @@ resource "local_file" "keySave" {
     filename = "mykey2.pem"
 }
 
+
+// Launch ec2 instance
 resource "aws_instance" "web" {
     ami           = "ami-052c08d70def0ac62"
     instance_type = "t2.micro"
@@ -70,6 +74,7 @@ resource "aws_instance" "web" {
     ]
 }
 
+// Create EFS filesystem
 resource "aws_efs_file_system" "my_personal_efs" {
   creation_token = "my-efs"
 
@@ -78,8 +83,35 @@ resource "aws_efs_file_system" "my_personal_efs" {
   }
 }
 
+// Mount EFS filesystem to instance
 resource "aws_efs_mount_target" "efs-mount" {
+   depends_on = [
+   	aws_efs_file_system.my_personal_efs,
+   ]
    file_system_id  = "${aws_efs_file_system.my_personal_efs.id}"
    subnet_id = "${aws_instance.web.subnet_id}"
    security_groups = ["${aws_security_group.efsSecurityGroup.id}"]
  }
+ 
+// Mount EFS filesystem to directory
+resource "null_resource" "NFSConfig" {
+    depends_on = [
+    aws_efs_mount_target.efs-mount,
+    aws_instance.web,
+    ]
+    connection {
+        type     = "ssh"
+        user     = "ec2-user"
+        private_key = tls_private_key.keyGenerate.private_key_pem
+        host     = aws_instance.webInstance.public_ip
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+            "sudo yum install nfs-utils -y",
+            "sudo mkdir /myfolder",
+            "sudo mount -t nfs4 ${aws_efs_mount_target.EFSMount.dns_name}:/  /myfolder", 
+        ]
+    }
+}
+
